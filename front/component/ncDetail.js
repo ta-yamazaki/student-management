20278be8af22e0dcf040431b470dfb3568ce74f6
yamazaki <takashi.yamazaki@aideal.world>
2020-12-27 20:11:31 +0900

    detail画面UI作成

--- front/component/ncDetail.js
+++ front/component/ncDetail.js
@@ -1,26 +1,198 @@
 
+const bsList = [
+    "B導入", "ペテロ", "ヨシュア",
+    "エリヤ", "人間", "七段階",
+    "時代性", "比喩", "火と終末",
+    "洪水", "三位一体", "無知",
+    "予定", "中心人物", "サタン",
+    "啓示", "再臨･引上", "カイン",
+    "異端", "創造目的", "堕落",
+    "霊界", "復活", "罪･悔改",
+    "御子", "ひと時", "オリブ",
+    "千年王国", "再臨実相", "二人の証人"
+];
+
+
+Vue.component('bs-status', {
+  data: function () {
+    return {
+            selectedBs: [],
+            bsList: [],
+            loader: null,
+            loading: false,
+    }
+  },
+    created() {
+        this.bsList = bsList;
+    },
+    methods: {
+    },
+    watch: {
+        loader () {
+            const l = this.loader
+            this[l] = !this[l]
+            setTimeout(() => (this[l] = false), 3000)
+            this.loader = null
+        },
+    },
+  template: `
+      <v-card-text>
+            <v-row
+                dense no-gutters
+                class="text-no-wrap"
+            >
+                <v-col
+                    cols="4"
+                    v-for="bs in bsList"
+                    :key="bs"
+                >
+                  <v-checkbox
+                   v-model="selectedBs"
+                   :label="bs" :value="bs"
+                   class="my-0 py-0"
+                  ></v-checkbox>
+                </v-col>
+            </v-row>
+
+            <v-btn
+              block
+              :loading="loading"
+              :disabled="loading"
+              color="info"
+              @click="loader = 'loading'"
+            >
+            更新
+            </v-btn>
+        </v-card-text>
+        `
+});
+
+Vue.component('nc-profile', {
+  data: function () {
+    return {
+            loader: null,
+            loading: false,
+            rules: [v => v.length <= 200 || '最大200文字まで'],
+    }
+  },
+  props: ['profile'],
+    created() {
+    },
+    methods: {
+    },
+    watch: {
+        loader () {
+            const l = this.loader
+            this[l] = !this[l]
+            setTimeout(() => (this[l] = false), 3000)
+            this.loader = null
+        },
+    },
+  template: `
+      <v-card-text>
+          <v-textarea
+            v-model="profile.memo"
+            auto-grow clearable
+            counter filled
+            label="メモ"
+            counter="200"
+            :rules="rules"
+            :loading="loading"
+          ></v-textarea>
+
+          <v-btn
+            block
+            :loading="loading"
+            :disabled="loading"
+            color="info"
+            @click="loader = 'loading'"
+          >
+          更新
+          </v-btn>
+      </v-card-text>
+      `
+});
+
 var ncDetail = {
     data: function () {
         return {
+            pageTitle: '',
+            tab: null,
             profile: {},
-            bsStatus: {}
        }
     },
     created() {
         const ncId = this.$route.params.id;
-        axios.get("/api/nc/detail/" + ncId).then(response => {
-            this.profile = response.data;
+        axios.get("/api/nc/detail/" + ncId).then(res => {
+            this.profile = res.data;
+            this.pageTitle = res.data.name;
         });
     },
     methods: {
     },
-    template: `<div>
-                <div>{{ profile.name }}</div>
-                <div>{{ profile.belongs }}</div>
-                <div>{{ profile.memo }}</div>
-               </div>`
+    watch: {
+        loader () {
+            const l = this.loader
+            this[l] = !this[l]
+            setTimeout(() => (this[l] = false), 3000)
+            this.loader = null
+        },
+    },
+    template: `
+        <v-flex xs12 sm12 md10 lg8 xl8 class="mx-auto">
+
+            <v-list>
+                <v-app-bar
+                    dense fixed
+                    elevation="3" elevate-on-scroll
+                    color="grey lighten-5" height="38"
+                >
+                    <v-btn
+                      icon
+                      v-show="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm"
+                      :to="{ path: '/' }"
+                    >
+                      <v-icon>mdi-chevron-left</v-icon>
+                    </v-btn>
+
+                    <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
+                </v-app-bar>
+            </v-list>
+
+
+           <v-card-text>
+                <v-tabs-items v-model="tab">
+                  <v-tab-item>
+                    <v-card flat>
+                          <bs-status></bs-status>
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-card flat>
+                          <nc-profile :profile="profile"></nc-profile>
+                    </v-card>
+                  </v-tab-item>
+                </v-tabs-items>
+            </v-card-text>
+
+            <v-bottom-navigation grow fixed height="48">
+                <v-tabs
+                  v-model="tab"
+                  background-color="grey lighten-5"
+                      color="primary"
+                  grow
+                >
+                  <v-tab>BS状況</v-tab>
+                  <v-tab>プロフィール</v-tab>
+                </v-tabs>
+              </v-bottom-navigation>
+        </v-flex>
+   `
 };
 
 export default ncDetail;
+
+
+
 
 
--- front/component/ncList.js
+++ front/component/ncList.js
@@ -6,69 +6,71 @@ var ncList = {
         search: '',
         item: -1, //デフォルトでアクティブにするアイテム（index）
         items: [],
-        disabled: false,
-        dense: false,
         twoLine: true,
-        threeLine: false,
-        shaped: false,
-        flat: false,
-        subheader: false,
-        inactive: false,
-        subGroup: false,
-        nav: false,
         avatar: true,
-        rounded: false,
       }
     },
     created() {
-        axios.get("/api/nc/list").then(response => {
-            this.items = response.data;
+        axios.get("/api/nc/list").then(res => {
+            this.items = res.data;
         });
     },
+    computed: {
+        filteredNcList(){
+        　return this.items.filter(nc => {
+            var targetText = nc.name + nc.belongs + nc.memo;
+            return targetText.includes(this.search);
+        　})
+        }
+    },
     methods: {
+        iconText(name) {
+            return name.charAt(0);
+        }
     },
     template: `
-        <v-container>
-            <v-row align="center">
-                <v-card class="mx-auto" tile>
-                    <v-toolbar dense elevation="1">
-                      <!--  <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-                        <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
-                        <v-spacer></v-spacer>
-                        <v-spacer></v-spacer>
-                         <v-text-field
-                             v-model="search"
-                             label="search"
-                             type="text"
-                             clearable dense
-                             hide-details single-line
-                             prepend-inner-icon="mdi-account-search-outline"
-                           ></v-text-field>
-                    </v-toolbar>
+        <v-flex xs12 sm12 md10 lg8 xl8 class="mx-auto">
+                <v-app-bar
+                    dense fixed
+                    elevation="3" elevate-on-scroll
+                    color="grey lighten-5" height="38"
+                >
+                  <!--  <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+                    <v-toolbar-title>{{ pageTitle }}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-spacer></v-spacer>
+                     <v-text-field
+                         v-model="search"
+                         label="search"
+                         type="text"
+                         clearable dense
+                         hide-details single-line
+                         prepend-inner-icon="mdi-magnify "
+                       ></v-text-field>
+                </v-app-bar>
 
-                    <v-list :two-line="twoLine" :avatar="avatar">
-                        <v-list-item-group v-model="item" color="primary">
-                            <template v-for="(item, i) in items">
-                                <v-divider v-if="i!=0" inset></v-divider>
-                                <v-list-item
-                                    :key="i"
-                                    :inactive="inactive"
-                                    :to="{ path: '/nc/detail/' + item.id }"
-                                >
-                                    <v-list-item-avatar v-if="avatar">
-                                        <v-img :src="item.avatar"></v-img>
-                                    </v-list-item-avatar>
-                                    <v-list-item-content>
-                                        <v-list-item-title v-html="item.name + '<small>（' + item.belongs + '）</small>'"></v-list-item-title>
-                                        <v-list-item-subtitle v-if="twoLine || threeLine" v-html="item.memo"></v-list-item-subtitle>
-                                    </v-list-item-content>
-                                </v-list-item>
-                            </template>
-                        </v-list-item-group>
-                    </v-list>
-                </v-card>
-            </v-row>
-        </v-container>
+            <v-list twoLine avatar>
+                <v-list-item-group v-model="item" color="primary">
+                    <template v-for="(item, i) in filteredNcList">
+                        <v-divider v-if="i!=0" inset></v-divider>
+                        <v-list-item
+                            :key="i"
+                            :to="{ path: '/nc/detail/' + item.id }"
+                        >
+                            <v-list-item-avatar v-if="avatar">
+                                <v-avatar :color="item.avatar.color">
+                                      <span class="white--text headline">{{ iconText(item.name) }}</span>
+                                    </v-avatar>
+                            </v-list-item-avatar>
+                            <v-list-item-content>
+                                <v-list-item-title v-html="item.name + '<small>（' + item.belongs + '）</small>'"></v-list-item-title>
+                                <v-list-item-subtitle v-if="twoLine || threeLine" v-html="item.memo"></v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </template>
+                </v-list-item-group>
+            </v-list>
+        </v-flex>
     `
 };
 
